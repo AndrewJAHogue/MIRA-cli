1f194bb71baab99e533ae07fdf1365ab8348224f
Andrew Hogue <andrewjahogue@gmail.com>
2022-06-29 18:43:42 -0500

    lots of new methods

--- gaussian_fit.py
+++ gaussian_fit.py
@@ -1,3 +1,4 @@
+import shutil
 import astropy.nddata.utils as utils
 from lineplots import GetNthColumn, GetNthRow
 import numpy as np
@@ -6,6 +7,7 @@ from sympy.abc import A,a,x,y,c,b
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from scipy.interpolate import  griddata
+import astropy.stats as stats
 
 from astropy.utils.exceptions import AstropyUserWarning
 from astropy.modeling import models, fitting
@@ -13,35 +15,40 @@ import warnings
 
 import lmfit
 from lmfit.lineshapes import  gaussian2d, lorentzian
-
-basePath = '/media/al-chromebook/USB20FD/Python/Research/fits/Full Maps/'
-# basePath = '/media/al-linux/USB20FD/Python/Research/fits/Full Maps/'
-sofia = 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
-sofia_full = basePath + 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
-hdu = fits.open(sofia_full)[0]
-sofia_full_image = hdu.data
-
 import time
 import os.path
 
 import json
 
+computer_path = '/media/al-chromebook/'
+# computer_path = '/media/al-linux/'
 
-new_dict = {}
 
+def MoffatFit(coords, file, cutoutSize, saveFigs=False, showPlot = True):
+    file_hdu = fits.open(file)[0]
+    file_data = file_hdu.data
 
-def MoffatFit(coords):
-    # inner_dict['file'] = sofia
-    # inner_dict[f'{coords}']['file'] = sofia
-    # new_dict[f'{coords}'] = {'file': sofia}
+    new_dict = {}
+
+    filename = spliceFullPath(file) + '_MoffatFit'
+    makeNewFolder(filename)
+    
+    base_figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}'
+
+    json_file = figPath +  '/data.json'
+    # Create the initial json
+    if os.path.isfile(json_file) == False:
+        shutil.copy2(base_figPath + 'data.json', figPath)
 
     from astropy.nddata import Cutout2D 
-    size = 30
-    x,y = np.mgrid[:size, :size]
-    data = sofia_full_image
-    Data = Cutout2D(data, (coords), size).data
+    x,y = np.mgrid[:cutoutSize, :cutoutSize]
+    Data = Cutout2D(file_data, (coords), cutoutSize).data
+    mean, median, tmp = stats.sigma_clipped_stats(Data)
+    Data -= median
 
-    p_init = models.Moffat2D(x_0=size / 2, y_0=size / 2,amplitude=np.nanmax(Data) )
+    p_init = models.Moffat2D(x_0=cutoutSize / 2, y_0=cutoutSize / 2,amplitude=np.nanmax(Data) )
+    # p_init = models.Gaussian2D(x_mean=15, y_mean=15)
     fit_p = fitting.LevMarLSQFitter()
 
     with warnings.catch_warnings():
@@ -49,9 +56,19 @@ def MoffatFit(coords):
         p = fit_p(p_init, x, y, Data)
 
     fwhm = p.fwhm*.768
+    # fwhm = p.fwhm *3.2
+
     print(f'{coords=} and {fwhm=}')
     new_dict['fwhm'] = fwhm
     new_dict[f'amplitude'] = f'{ p.amplitude }'
+
+    # fwhm_x = p.x_fwhm
+    # fwhm_y = p.y_fwhm
+    # print(f'{fwhm_x=} and {fwhm_y}')
+    # new_dict['fwhm_x'] = fwhm_x
+    # new_dict['fwhm_y'] = fwhm_y
+
+
 
     # Plot the Data with the best-fit model
     plt.figure(figsize=(8, 2.5))
@@ -62,28 +79,144 @@ def MoffatFit(coords):
     plt.imshow(p(x, y), origin='lower', interpolation='nearest')
     plt.title("Model")
     plt.subplot(1, 3, 3)
-    plt.imshow(Data - ( p(x, y)*1.125 ), origin='lower', interpolation='nearest')
+    plt.imshow(Data - ( p(x, y)), origin='lower', interpolation='nearest')
     plt.title("Residual")
 
-    # basePath = '/media/al-chromebook/USB20FD/MIRA-CLI/Figures/'
-    # basePath = '/media/al-linux/USB20FD/MIRA-CLI/Figures/'
-    # newPath = f'{basePath}{sofia}/'
-    # if os.path.isdir(newPath) == False:
-    #     os.mkdir(newPath)
+
+
+    if saveFigs:
+        SaveFig(filename, coords)
+
+    dumpInfo(filename, new_dict, coords)
+
+    if showPlot:
+        plt.show()
+
+
+def Gauss2D(coords, file, cutoutSize, saveFigs=False, showPlot = True):
+    file_hdu = fits.open(file)[0]
+    file_data = file_hdu.data
+
+    new_dict = {}
+
+    filename = spliceFullPath(file) + '_Gauss2D'
+    makeNewFolder(filename)
+
+    base_figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}'
+
+    json_file = figPath +  '/data.json'
+    # Create the initial json
+    if os.path.isfile(json_file) == False:
+        shutil.copy2(base_figPath + 'data.json', figPath)
+
+    from astropy.nddata import Cutout2D 
+    x,y = np.mgrid[:cutoutSize, :cutoutSize]
+    Data = Cutout2D(file_data, (coords), cutoutSize).data
+    mean, median, tmp = stats.sigma_clipped_stats(Data)
+    Data -= median
+
+    # p_init = models.Moffat2D(x_0=size / 2, y_0=size / 2,amplitude=np.nanmax(Data) )
+    p_init = models.Gaussian2D(x_mean=15, y_mean=15)
+    fit_p = fitting.LevMarLSQFitter()
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', message='Model is linear in parameters', category=AstropyUserWarning)
+        p = fit_p(p_init, x, y, Data)
+
+    fwhm_x = p.x_fwhm*3.2
+    fwhm_y = p.y_fwhm*3.2
+    print(f'{fwhm_x=} and {fwhm_y=} and fwhm={np.sqrt(fwhm_x*fwhm_y)}')
+    new_dict['fwhm'] = np.sqrt(fwhm_x*fwhm_y)
+    new_dict['fwhm_x'] = fwhm_x
+    new_dict['fwhm_y'] = fwhm_y
+
+
+
+    # Plot the Data with the best-fit model
+    plt.figure(figsize=(8, 2.5))
+    plt.subplot(1, 3, 1)
+    plt.imshow(Data, origin='lower', interpolation='nearest')
+    plt.title("Data")
+    plt.subplot(1, 3, 2)
+    plt.imshow(p(x, y), origin='lower', interpolation='nearest')
+    plt.title("Model")
+    plt.subplot(1, 3, 3)
+    plt.imshow(Data - ( p(x, y)), origin='lower', interpolation='nearest')
+    plt.title("Residual")
+
+
+
+    if saveFigs:
+        SaveFig(filename, coords)
+
+    dumpInfo(filename, new_dict, coords)
+
+    if showPlot:
+        plt.show()
+
+
+def makeNewFolder(filename):
+    basePath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+    newPath = f'{basePath}{filename}/'
+
+    if os.path.isdir(newPath) == False:
+        os.mkdir(newPath)
+
+def SaveFig(filename, coords):
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}/'
+
     # newFile = time.strftime('%Y%m%d-%H%M%S')
-    # newFile = f'{newPath}{newFile}_X{coords[0]}_Y{coords[1]}.png'
-    # plt.savefig(newFile)
-    # json_dict[f'{ coords }'].add(inner_dict)
+    newFile = f'{figPath}_X{coords[0]}_Y{coords[1]}.png'
+    plt.savefig(newFile)
 
+def dumpInfo(filename, info, coords):
+    base_figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}'
+
+    json_file = figPath +  '/data.json'
+    # Create the initial json
+    if os.path.isfile(json_file) == False:
+        shutil.copy2(base_figPath + 'data.json', figPath)
     ## save the data to a json
-    # with open(newPath + 'data.json', 'r+') as file:
-        # file_data = json.load(file)
+    with open(json_file, 'r+') as f:
+        f_data = json.load(f)
 
-        # file_data.update({f'{coords}': new_dict})
-        # file.seek(0)
-        # json.dump(file_data, file, indent=4)
+        f_data.update({f'{coords}': info})
+        f.seek(0)
+        json.dump(f_data, f, indent=4)
 
-    plt.show()
-    
+def spliceFullPath(file):
+    index = file.rfind('/') + 1
+    return file[index:]
 
-# MoffatFit((1145, 3415))
+# __________________Parameters______________________________________________________________
+
+combinedPath = '/media/al-chromebook/USB20FD/Python/Research/fits/Combined Maps/'
+basePath = f'{computer_path}USB20FD/Python/Research/fits/Full Maps/'
+
+
+# file = 'full_0.0147_2026.fits.fits'
+# full_map = computer_path + file 
+# spits_hdu = fits.open(full_map)[0]
+# full_map_data = spits_hdu.data
+# full_map_data = np.nan_to_num(full_map_data, posinf=np.nan)
+
+
+# sofia = 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
+# sofia = basePath + sofia
+
+# ___________________Args______________________________________________________________
+
+# coords = 3115, 298
+# size = 30
+
+# ___________________EXECECUTION______________________________________________
+
+# MoffatFit(coords, sofia, size, False, False)
+
+
+# notes
+# compare spits, smear sofia until it matches spits
+# then go on to coadding
+# 8-10 point sources in spits, ~6 fwhm
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import openpyxl as pe
 import gaussian_fit
+import numpy as np
+
+from astropy.io import fits
+
+# __________________Parameters______________________________________________________________
+computer_path = '/media/al-chromebook/'
+Combined_path = '/media/al-chromebook/USB20FD/Python/Research/fits/Combined Maps/'
+basePath = f'{computer_path}USB20FD/Python/Research/fits/Full Maps/'
+
+
+# file = 'full_0.0147_2026.fits.fits'
+# full_map = computer_path + file 
+# spits_hdu = fits.open(full_map)[0]
+# full_map_data = spits_hdu.data
+# full_map_data = np.nan_to_num(full_map_data, posinf=np.nan)
+
 
 
 def csv():
-    basePath = '/media/al-chromebook/USB20FD/'
+    sofia = 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
+    sofia = basePath + sofia
+
+    # basePath = '/media/al-chromebook/USB20FD/'
     # basePath = '/media/al-linux/USB20FD/'
     filename = "SOFIA pointsources"
-    data = pe.load_workbook(f'{basePath}{filename}.xlsx')
+    data = pe.load_workbook(f'{computer_path}USB20FD/{filename}.xlsx')
     sheet = data.active
     # x_cells, y_cells = sheet['B'][49:52], sheet['C'][49:52]
     x_cells, y_cells = sheet['B'][49:101], sheet['C'][49:101]
@@ -15,8 +34,97 @@ def csv():
         if 'Corrected' in sheet['I'][index_x + 49].value:
             coords = (x.value, y_cells[index_x].value)
             print(f'{coords=}')
-            gaussian_fit.MoffatFit(coords)
+            gaussian_fit.MoffatFit(coords, sofia, 30, True, False)
+            gaussian_fit.Gauss2D(coords, sofia, 30, True, False)
 
+def spits_points():
+    spits_point_sources = [
+        [52,115],
+        [35,35],
+        [288, 328],
+        [258, 337],
+        [250, 81],
+        [80, 89],
+        [90, 309],
+        [28, 327]
+    ]
+            
+
+    size = 30
+    baseFits_path = '/media/al-chromebook/USB20FD/Python/Research/fits/'
+
+    sofia = 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
+    sofia_path = f'Full Maps/{sofia}' 
+
+    spits_isofield = 'Spitzer24_IsoFields.fits'
+    forcast_isofield1= 'Forcast25_isoField1.fits'
+
+    chosen_file = baseFits_path + spits_isofield
+
+    for coords in spits_point_sources:
+        # gaussian_fit.MoffatFit(coords, chosen_file, size, True, False)
+        gaussian_fit.Gauss2D(coords, chosen_file, size, True, False)
+
+
+def std_of_fwhm(json_file):
+    import json
+    all_fwhm = []
+    with open(json_file, 'r+') as f:
+        f_data = json.load(f)
+
+        for key in f_data:
+            all_fwhm.append(f_data[key]['fwhm'])
         
         
-csv()
+    return np.std(all_fwhm)
+
+def avg_fwhm(json_file):
+    import json
+    all_fwhm = []
+    with open(json_file, 'r+') as f:
+        f_data = json.load(f)
+
+        for key in f_data:
+            all_fwhm.append(f_data[key]['fwhm'])
+        
+        
+    return np.average(all_fwhm)
+            
+def spits_iso_std():
+    base_figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+
+    spits_isofield = 'Spitzer24_IsoFields.fits'
+
+    filename = spits_isofield + '_Gauss2D'
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}'
+
+    json_file = figPath +  '/data.json'
+    print(f'{spits_isofield}_stdev = { std_of_fwhm(json_file) }')
+
+    return std_of_fwhm(json_file)
+
+def spits_iso_avg_fwhm():
+    base_figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/'
+
+    spits_isofield = 'Spitzer24_IsoFields.fits'
+
+    filename = spits_isofield + '_Gauss2D'
+    figPath = f'{computer_path}/USB20FD/MIRA-CLI/Figures/{filename}'
+
+    json_file = figPath +  '/data.json'
+    print(f'{spits_isofield}_average = { avg_fwhm(json_file)}')
+
+def spits_iso_avg():
+
+    baseFits_path = '/media/al-chromebook/USB20FD/Python/Research/fits/'
+    spits_isofield = 'Spitzer24_IsoFields.fits'
+
+    data = fits.open(baseFits_path + spits_isofield)[0].data
+
+    stdev = np.std(data)
+    average = np.average(data)
+    print(f'{stdev=}')
+    print(f'{average=}')
+
+
+spits_iso_avg()
