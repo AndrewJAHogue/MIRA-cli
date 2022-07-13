d88883e3f66d04b0be6fa5da14deb5041755f227
AndrewJAHogue <andrewjahogue@gmail.com>
2022-07-13 13:21:24 -0500

    added coadd method

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ def smoothen_sofia():
     # fwhm *= 3.2 ## ONLY FOR SPITZER FILES
 
     kernel = Gaussian2DKernel(1.15)
-    astropy_conv = convolve_fft(original_data, kernel, allow_huge=True)
+    astropy_conv = convolve_fft(original_data, kernel, allow_huge=True) 
     fwhm_conv = get_fwhm_gauss_data((coords), astropy_conv, cutoutSize)['fwhm']
     # fwhm_conv *= 3.2 ## ONLY FOR SPITZER FILES
 
@@ -396,26 +396,26 @@ def smoothen_sofia():
     org_cutout = Cutout2D(original_data, (coords), cutoutSize).data
     conv_cutout = Cutout2D(astropy_conv, (coords), cutoutSize).data
 
-    # # mean, median, tmp = stats.sigma_clipped_stats(org_cutout)
-    # x, y = GetNthColumn(org_cutout, round(cutoutSize/2))
-    # plt.subplot(121)
-    # plt.title(f'Original Data {fwhm=}')
-    # plt.plot(x, y)
+    # mean, median, tmp = stats.sigma_clipped_stats(org_cutout)
+    x, y = GetNthColumn(org_cutout, round(cutoutSize/2))
+    plt.subplot(121)
+    plt.title(f'Original Data {fwhm=}')
+    plt.plot(x, y)
 
-    # x, y = GetNthColumn(conv_cutout, round(cutoutSize/2))
-    # plt.subplot(122)
-    # plt.title(f'Convolved {fwhm_conv=}')
-    # plt.plot(x, y)
+    x, y = GetNthColumn(conv_cutout, round(cutoutSize/2))
+    plt.subplot(122)
+    plt.title(f'Convolved {fwhm_conv=}')
+    plt.plot(x, y)
 
     print(f'Percent difference = {100 - fwhm/fwhm_conv * 100 =}')
 
 
-    plt.subplot(131)
-    plt.imshow(org_cutout)
-    plt.subplot(132)
-    plt.imshow(conv_cutout)
-    plt.subplot(133)
-    plt.imshow(org_cutout - conv_cutout)
+    # plt.subplot(131)
+    # plt.imshow(org_cutout)
+    # plt.subplot(132)
+    # plt.imshow(conv_cutout)
+    # plt.subplot(133)
+    # plt.imshow(org_cutout - conv_cutout)
 
 
     plt.show()
@@ -432,6 +432,7 @@ def get_json_info(filepath):
             new_dict.append(key)
     return new_dict
 
+# np.average(org_fwhms)=5.015014581927903 and np.average(conv_fwhms)=6.4360830539570335
 def loop_smoothen_sofia():
     from astropy.stats import gaussian_fwhm_to_sigma
     from os.path import exists
@@ -488,26 +489,156 @@ def loop_smoothen_sofia():
     import astropy.stats as stats
     from lineplots import GetNthColumn, GetNthRow
     # original map
-    original_data[np.isnan(original_data)] = 0
-    original_data[np.isinf(original_data)] = 0
+    # original_data[np.isnan(original_data)] = 0
+    # original_data[np.isinf(original_data)] = 0
+    kernel = Gaussian2DKernel(1.20)
+    astropy_conv = convolve_fft(original_data, kernel, allow_huge=True)
+    coadd_data = np.nansum([astropy_conv, original_data], axis=0)  + 0.05
     # org_cutout -= median
     org_fwhms = []
     conv_fwhms = []
+    coadd_fwhms = []
     for point in new_sofia_points:
         print(f'\n{point=}')
         fwhm = get_fwhm_gauss_data((point), original_data, cutoutSize)['fwhm']
         org_fwhms.append(fwhm)
 
-        kernel = Gaussian2DKernel(1.20)
-        astropy_conv = convolve_fft(original_data, kernel, allow_huge=True)
         print('\nAnd the convolved point data is::')
         fwhm_conv = get_fwhm_gauss_data((point), astropy_conv, cutoutSize)['fwhm']
+        fwhm_coadd = get_fwhm_gauss_data((point),coadd_data , cutoutSize)['fwhm']
         conv_fwhms.append(fwhm_conv)
+        coadd_fwhms.append(fwhm_coadd)
         # fwhm_conv *= 3.2 ## ONLY FOR SPITZER FILES
 
     
-    print(f'{np.average(org_fwhms)=} and {np.average(conv_fwhms)=}')
+    print(f'\n{np.nanmean(org_fwhms)=} and {np.nanmean(conv_fwhms)=} and {np.nanmean(coadd_fwhms)=}')
 
+
+def coadd():
+    from astropy.stats import gaussian_fwhm_to_sigma
+    from os.path import exists
+    from math import floor
+    from random import gauss
+    import numpy as np
+
+    from astropy.io import fits
+    from astropy.convolution import Gaussian2DKernel, convolve, interpolate_replace_nans, convolve_fft 
+
+    # baseFits_path = f'{computer_path}/USB20FD/Python/Research/fits/'
+    baseFits_path = f'{computer_path}/Python/Research/fits/'
+    org_path = 'Full Maps/Originals/'
+    spits_full = baseFits_path + org_path + 'gcmosaic_24um.fits'
+    spits_fitted = baseFits_path + 'Full Maps/Spitzer_GCmosaic_24um_onFORCASTheader_JyPix.fits'
+    sofia_full = baseFits_path + org_path + 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
+    # spits_isofield = 'Spitzer24_IsoFields.fits'
+
+    original_data = fits.open(sofia_full)[0].data
+    spitz_data = fits.open(spits_fitted)[0].data
+
+    sofia_point_sources = [
+        [3115,297],
+        [2125,3117],
+        [1339,2698],
+        [958,3374],
+        [1145,3417],
+        [1174,4061],
+        [1160,4201],
+        [542,4930],
+        [614,4941],
+        [565,5025]
+    ] 
+    spits_point_sources = [
+        [52,115],
+        [35,35],
+        [288, 328],
+        [258, 337],
+        [250, 81],
+        [80, 89],
+        [90, 309],
+        [28, 327]
+    ]
+    # x, y
+    # coords = 3115, 298
+    # coords = 791, 4718
+    # coords =  3080, 372
+    coords = sofia_point_sources[0]
+    from gaussian_fit import get_fwhm_gauss_file, get_fwhm_gauss_data
+    # coords = 227, 154
+    cutoutSize = 15
+
+
+
+    from astropy.nddata import Cutout2D 
+    import astropy.stats as stats
+    from lineplots import GetNthColumn, GetNthRow
+    # original map
+    # original_data[np.isnan(original_data)] = 0
+    # original_data[np.isinf(original_data)] = 0
+    # org_cutout -= median
+    fwhm = get_fwhm_gauss_data((coords), original_data, cutoutSize)['fwhm']
+    
+    # fwhm *= 3.2 ## ONLY FOR SPITZER FILES
+
+    kernel = Gaussian2DKernel(1.15)
+    astropy_conv = convolve_fft(original_data, kernel, allow_huge=True) 
+    fwhm_conv = get_fwhm_gauss_data((coords), astropy_conv, cutoutSize)['fwhm']
+    coadd_data = np.nansum([astropy_conv, original_data], axis=0)  + 0.05
+    # fwhm_conv *= 3.2 ## ONLY FOR SPITZER FILES
+
+    
+    # org_cutout = Cutout2D(original_data, (coords), cutoutSize).data
+    # conv_cutout = Cutout2D(astropy_conv, (coords), cutoutSize).data
+
+    coords = 490, 4815
+    # coords = spits_point_sources[3]
+    spits_cutout = Cutout2D(spitz_data, (coords), cutoutSize).data
+    coadd_cutout = Cutout2D(coadd_data, (coords), 100).data
+    # x, spits_y = GetNthColumn(spitz_data, coords[0])
+    # plt.plot(x, spits_y, label='Spitzer')
+
+    # x, conv_y = GetNthColumn(astropy_conv, coords[0])
+    # plt.plot(x, conv_y, label='Convolved Sofia')
+    # x, coadd_y = GetNthColumn(coadd_data, coords[0])
+    # plt.plot(x, coadd_y, label='Coadd')
+    # plt.legend()
+
+    get_fwhm_gauss_data((coords),spitz_data, cutoutSize)['fwhm']
+    get_fwhm_gauss_data((coords),coadd_data, 100)['fwhm']
+    # print(f'Percent difference = {100 - fwhm/fwhm_conv * 100 =}')
+
+    plt.subplot(131)
+    plt.imshow(spits_cutout)
+    plt.subplot(132)
+    plt.imshow(coadd_cutout)
+
+
+    plt.show()
+
+def saveCoadd():
+    from astropy.stats import gaussian_fwhm_to_sigma
+    from os.path import exists
+    from math import floor
+    from random import gauss
+    import numpy as np
+
+    from astropy.io import fits
+    from astropy.convolution import Gaussian2DKernel, convolve, interpolate_replace_nans, convolve_fft 
+
+    # baseFits_path = f'{computer_path}/USB20FD/Python/Research/fits/'
+    baseFits_path = f'{computer_path}/Python/Research/fits/'
+    org_path = 'Full Maps/Originals/'
+    spits_full = baseFits_path + org_path + 'gcmosaic_24um.fits'
+    spits_fitted = baseFits_path + 'Full Maps/Spitzer_GCmosaic_24um_onFORCASTheader_JyPix.fits'
+    sofia_full = baseFits_path + org_path + 'F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
+    # spits_isofield = 'Spitzer24_IsoFields.fits'
+
+    original_data = fits.open(sofia_full)[0].data
+    spitz_data = fits.open(spits_fitted)[0].data
+
+    kernel = Gaussian2DKernel(1.15)
+    astropy_conv = convolve_fft(original_data, kernel, allow_huge=True) 
+
+    coadd_data = np.nansum([astropy_conv, spitz_data], axis=0)
 
 
 # np.average(org_fwhms)=5.015014581927903 and np.average(conv_fwhms)=6.4360830539570335
